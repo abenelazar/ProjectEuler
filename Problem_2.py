b6100e739d596d1941115a19085afb698a426bc3
Ari Ben-Elazar <abenelazar@gmail.com>
2015-12-30 22:55:43 -0500

    added comments

--- Problem_2.py
+++ Problem_2.py
@@ -1,19 +1,22 @@
-from numpy import linalg as LA
+from numpy.linalg import matrix_power
 import numpy as np
 
 def fib(n):
 	fib_mat = np.array([[1,1],[1,0]])
-	return LA.matrix_power(fib_mat, n)
-limit = 4000000
-inc = fib(3)
-mat = fib(2)
-total = 0
-while mat.max() < limit:
-	for row in mat:
-		for num in row:
-			if num % 2 == 0:
-				total += num
-	mat = np.dot(mat, inc)
+	return matrix_power(fib_mat, n)
 
-print total
+
+limit = 4000000
+cumulative_total = 0
+inc = fib(3) # increment matrix for fib_mat
+fib_mat = fib(2) # fib 1, 2, 3 
+
+while fib_mat.max() < limit:
+	for row in fib_mat:
+		for entry in row:
+			if entry % 2 == 0: # only evens
+				cumulative_total += entry
+	fib_mat = np.dot(fib_mat, inc) # next matrix with entries i-1, i, i+1
+
+print cumulative_total # ans
 
